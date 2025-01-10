bb873af3732aca02e305e76b5d23550afa41db40
munazhairfan <munazhairfan@gmail.com>
2025-01-10 19:57:47 +0500

    added breakpoints

--- app/side/page.tsx
+++ app/side/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
@@ -61,66 +60,3 @@ const Page = async () => {
 };
 
 export default Page;
-=======
-import React from "react";
-import Image from "next/image";
-import Link from "next/link";
-
-interface Meal {
-  idMeal: string;
-  strInstructions: string;
-  strMeal: string;
-  strMealThumb: string;
-  strArea: string;
-}
-const Page = async () => {
-  const response = await fetch(
-    "https://www.themealdb.com/api/json/v1/1/search.php?s=soup"
-  );
-  const data = await response.json();
-  const side: Meal[] = data.meals;
-
-  return (
-    <div>
-      <h1 className="text-8xl text-center font-dancing mb-4 mt-4">Side Dish</h1>
-      {side.map((food) => {
-        return (
-          <div className="py-8 flex flex-wrap md:flex-nowrap" key={food.idMeal}>
-            <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-              <Image
-                src={food.strMealThumb}
-                alt={food.idMeal}
-                height={400}
-                width={400}
-              ></Image>
-            </div>
-            <div className="md:flex-grow ml-5">
-              <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
-                {food.strMeal}
-              </h2>
-              <p className="leading-relaxed">{food.strInstructions.slice(0,500)}......</p>
-              <Link href={food.idMeal} className="text-indigo-500 inline-flex items-center mt-4">
-                Learn More
-                <svg
-                  className="w-4 h-4 ml-2"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path d="M5 12h14"></path>
-                  <path d="M12 5l7 7-7 7"></path>
-                </svg>
-              </Link>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
-
-export default Page;
->>>>>>> 6865bc71177856998400ac204b551de6417b1b0a
--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
@@ -26,33 +25,3 @@ const Header = () => {
 }
 
 export default Header
-=======
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-
-const Header = () => {
-  return (
-    <header className="text-gray-600 body-font bg-white shadow-lg">
-  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-      {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-green-500 rounded-full" viewBox="0 0 24 24">
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-      </svg> */}
-      <Image src={'/logo.jpg'} alt='logo' width={100} height={180}></Image>
-      <span className="ml-3 text-4xl font-extrabold font-dancing">Whisk & Wonder</span>
-    </a>
-    <nav className="md:ml-auto flex flex-wrap items-center text-2xl justify-center font-dancing">
-      <Link href={'./'} className="mr-16 hover:text-gray-900">Home</Link>
-      <Link href={'/recipes'} className="mr-16 hover:text-gray-900">Recipes</Link>
-      <Link href={'/comments'} className="mr-16 hover:text-gray-900">Reviews</Link>
-      <Link href={'/about'} className="mr-16 hover:text-gray-900">About</Link>
-    </nav>
-  </div>
-</header>
-  )
-}
-
-export default Header
-
->>>>>>> 6865bc71177856998400ac204b551de6417b1b0a
